6b53071451f4202b1da8afa5dc03a94031544a5f
RobertLikesCoding <rbaufeld@posteo.de>
2025-09-02 13:38:09 +0200

    adjust the dock apps

--- install/desktop/set-dock.sh
+++ install/desktop/set-dock.sh
@@ -2,23 +2,13 @@
 
 # Favorite apps for dock
 apps=(
-	"google-chrome.desktop"
-	"Alacritty.desktop"
-	"Neovim.desktop"
 	"code.desktop"
-	"WhatsApp.desktop"
-	"signal-desktop.desktop"
-	"Zoom.desktop"
-	"spotify.desktop"
+	"firefox_firefox.desktop"
+	"Alacritty.desktop"
+	"logseq.desktop"
 	"steam.desktop"
 	"pinta_pinta.desktop"
-	"md.obsidian.Obsidian.desktop"
-	"Activity.desktop"
 	"Docker.desktop"
-	"Omakub.desktop"
-	"1password.desktop"
-	"org.gnome.Settings.desktop"
-	"org.gnome.Nautilus.desktop"
 	"localsend_app.desktop"
 )
 
