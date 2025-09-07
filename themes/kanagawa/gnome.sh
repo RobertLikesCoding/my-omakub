b0a140fe38e6283740809a32a0257ef187fb8bbe
Robert Baufeld <rbaufeld@posteo.de>
2025-09-07 15:12:11 +0200

    change wallpapers for some themes

--- themes/kanagawa/gnome.sh
+++ themes/kanagawa/gnome.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 OMAKUB_THEME_COLOR="purple"
-OMAKUB_THEME_BACKGROUND="kanagawa/background.jpg"
+OMAKUB_THEME_BACKGROUND="kanagawa/background.png"
 source $OMAKUB_PATH/themes/set-gnome-theme.sh
