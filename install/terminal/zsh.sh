8edd60676d70780199cfaf7273671b1e4b99000f
Robert <rbaufeld@posteo.de>
2025-09-27 16:07:25 +0200

    add omakub command to zshrc

--- install/terminal/zsh.sh
+++ install/terminal/zsh.sh
@@ -3,3 +3,9 @@
 sudo apt install -y zsh
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Set complete path
+export PATH="./bin:$HOME/.local/bin:$HOME/.local/share/omakub/bin:$PATH"
+set +h
+
+export OMAKUB_PATH="/home/$USER/.local/share/omakub"
