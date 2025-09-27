775aa787147cf891d3f2f63feab705885d9b225c
Robert <rbaufeld@posteo.de>
2025-09-27 15:31:46 +0200

    add Bitwarden to optional apps and default it

--- install/desktop/optional/app-keepassxc.sh
+++ install/desktop/optional/app-keepassxc.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-sudo apt -y update
 sudo apt -y install keepassxc
--- install/first-run-choices.sh
+++ install/first-run-choices.sh
@@ -2,8 +2,8 @@
 
 # Only ask for default desktop app choices when running Gnome
 if [[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]]; then
-  OPTIONAL_APPS=("Logseq" "KeepassXC" "Cursor" "Signal")
-  DEFAULT_OPTIONAL_APPS="Logseq","KeepassXC"
+  OPTIONAL_APPS=("Logseq" "Bitwarden" "KeepassXC" "Cursor" "Signal")
+  DEFAULT_OPTIONAL_APPS="Logseq","Bitwarden"
   export OMAKUB_FIRST_RUN_OPTIONAL_APPS=$(gum choose "${OPTIONAL_APPS[@]}" --no-limit --selected $DEFAULT_OPTIONAL_APPS --height 7 --header "Select optional apps" | tr ' ' '-')
 fi
 
